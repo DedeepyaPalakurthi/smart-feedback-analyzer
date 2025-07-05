8fad229207f34c955116c5887cc6e0e0e4b3a95a
Dedeepya Palakurthi <dedeepyapalakurthi@gmail.com>
2025-07-05 11:00:54 -0400

    Add CI pipeline and test case

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_root_post():
+    response = client.post("/", json={"text": "I love this product!"})
+    assert response.status_code == 200
+    assert "label" in response.json()
